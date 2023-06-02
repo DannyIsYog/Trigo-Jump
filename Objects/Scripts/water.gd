2e92611759d0e84f8942133f7d6be8e42f173ed2
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-06-02 15:12:48 +0100

    bruh

--- Objects/Scripts/water.gd
+++ Objects/Scripts/water.gd
@@ -5,5 +5,5 @@ func _on_body_entered(body):
 	if not is_instance_of(body, PlayerBody):
 		return
 	var player_data = body.get_parent() as PlayerData
-	player_data.life = min(100, player_data.life + 5)
+	player_data.life = min(100, player_data.life + 8)
 	queue_free()
