4114fff2bf3a4eae6e14cd59401102a815f54e1e
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-06-02 12:35:42 +0100

    add better platform generation code

--- Objects/Scripts/game.gd
+++ Objects/Scripts/game.gd
@@ -1,17 +1,30 @@
 extends Node2D
 
 @export var platform_scene: PackedScene
+@export var player_scene: PackedScene
+
+@onready var platforms = $Platforms
+@onready var platform_spawn = $PlatformPath/Spawn
+@onready var platform_initial_y = $PlatformPath.position.y
 
 func _ready() -> void:
 	randomize()
-	spawn_platforms()
+	spawn_platforms(10, true)
 
 
-func spawn_platforms() -> void:
-	var spawn_paths = $PlatformSpawning.get_children()
-	for spawn_path in spawn_paths:
-		var platform: Node2D = platform_scene.instantiate()
-		var spawn_location: PathFollow2D = spawn_path.get_node("Spawn")
-		spawn_location.progress_ratio = randf()
-		platform.position = spawn_location.position
-		$Platforms.add_child(platform)
+func spawn_platforms(amount: int, spawn_player: bool = false) -> void:
+	for i in range(amount):
+		platform_initial_y -= randi_range(200, 328)
+		var platform = platform_scene.instantiate() as Node2D
+		
+		platform_spawn.progress_ratio = randf()
+		platform.position = platform_spawn.position
+		platform.position.y = platform_initial_y
+		
+		platforms.add_child(platform)
+		
+		if spawn_player and i == 0:
+			var player = player_scene.instantiate() as Node2D
+			player.position.x = platform.position.x + 64
+			player.position.y = platform_initial_y - 64
+			add_child(player)
