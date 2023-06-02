ff6a358ad1f31f85156eb4ec7f14311f0020e24f
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-06-02 12:55:46 +0100

    fix platforms spawning on top of previous

--- Objects/Scripts/game.gd
+++ Objects/Scripts/game.gd
@@ -6,20 +6,26 @@ extends Node2D
 @onready var platforms = $Platforms
 @onready var platform_spawn = $PlatformPath/Spawn
 @onready var platform_initial_y = $PlatformPath.position.y
+var last_platform_x = 0
 
 func _ready() -> void:
 	randomize()
-	spawn_platforms(10, true)
+	spawn_platforms(20, true)
 
 
 func spawn_platforms(amount: int, spawn_player: bool = false) -> void:
 	for i in range(amount):
-		platform_initial_y -= randi_range(200, 328)
+		platform_initial_y -= randi_range(296, 396)
 		var platform = platform_scene.instantiate() as Node2D
 		
 		platform_spawn.progress_ratio = randf()
+		var first_platform = i == 0 and spawn_player
+		while not first_platform and abs(platform_spawn.position.x - last_platform_x) <= 64:
+			platform_spawn.progress_ratio = randf()
+		
 		platform.position = platform_spawn.position
 		platform.position.y = platform_initial_y
+		last_platform_x = platform.position.x
 		
 		platforms.add_child(platform)
 		
