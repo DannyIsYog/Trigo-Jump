ffcc1f39977b9711e8fff72f64043e8a6fd0cd1a
Fábio "DannyIsYog" Sousa <fabio.sousa@tecnico.ulisboa.pt>
2023-06-02 13:57:48 +0100

    Fixed camera

--- Objects/Scripts/camera.gd
+++ Objects/Scripts/camera.gd
@@ -3,12 +3,14 @@ extends Camera2D
 var player = null
 @export var maxY: float
 
+var firstTime = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player == null:
 		player = get_parent().get_node("Player")
 		return
-	
+	if firstTime:
+		firstTimeSet()
 	var nextY = player.get_node("PlayerBody").get_global_position().y
 	if nextY < maxY:
 		self.position.y = nextY
@@ -16,4 +18,12 @@ func _process(delta):
 	var nextLimit = self.position.y + (get_viewport_rect().size.y / 2)
 	if nextLimit < self.limit_bottom :
 		self.limit_bottom = self.position.y + (get_viewport_rect().size.y / 2)
+
+func firstTimeSet():
+	var nextY = player.get_node("PlayerBody").get_global_position().y
+	self.position.y = nextY
+	maxY = nextY
+	var nextLimit = self.position.y + (get_viewport_rect().size.y / 2)
+	self.limit_bottom = self.position.y + (get_viewport_rect().size.y / 2)
+	firstTime = false
 	
--- Objects/Scripts/playerJump.gd
+++ Objects/Scripts/playerJump.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 @export var jump_speed: int
 @export var gravity: int
 @export var direction: int
-@onready var animationtree : AnimationTree = $"../AnimationTree"
 
 func _physics_process(delta):
 	# Add gravity every frame
@@ -18,9 +17,4 @@ func _physics_process(delta):
 	# Only allow jumping when on the ground
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
-	
-func _ready():
-	animationtree.active = true
-	
-func update_animation():
-	animationtree.set("parameters/Move/blend_position", direction)
+
