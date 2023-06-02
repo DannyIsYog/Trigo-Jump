7cf6509e7c445e093bbbeaded49aa94da9be2107
Zytr0nn <tomas.eufrasio.oliveira@gmail.com>
2023-06-02 18:54:06 +0100

    cenas

--- Objects/Scripts/wine.gd
+++ Objects/Scripts/wine.gd
@@ -7,4 +7,5 @@ func _on_body_entered(body):
 	var player_data = body.get_parent() as PlayerData
 	player_data.life = min(100, player_data.life + 16)
 	player_data.drunk_for = 5.0
+	body.WineSound.play()
 	queue_free()
