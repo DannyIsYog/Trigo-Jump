d6db4e74b837991b6c0f5e342354433a6fc5c75d
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2023-06-02 18:24:19 +0100

    fix Fábio's typo

--- Game/Scripts/score.gd
+++ Game/Scripts/score.gd
@@ -3,7 +3,7 @@ extends RichTextLabel
 @onready var gameManager = get_parent()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.text = "[center] Score: " + str(gameManager.score) + " m \n Hightscore: " + str(gameManager.highScore) + " m \n\n\n\n Press R to restart[/center]" 
+	self.text = "[center] Score: " + str(gameManager.score) + " m \n Highscore: " + str(gameManager.highScore) + " m \n\n\n\n Press R to restart[/center]" 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
